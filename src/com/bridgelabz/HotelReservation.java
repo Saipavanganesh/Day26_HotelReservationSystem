3e1558b782271975c080a03722c1776d72e7b216
Saipavanganesh <pavanganeshms@gmail.com>
2023-02-01 20:08:03 +0530

    Added hotels and rates for regular customer

--- src/com/bridgelabz/HotelReservation.java
+++ src/com/bridgelabz/HotelReservation.java
@@ -1,8 +1,13 @@
 package com.bridgelabz;
 
 public class HotelReservation {
-    public static void main(String[] args) {
+    public HotelReservation() {
         System.out.println("Welcome to Hotel Reservation Program");
         System.out.println("************************************");
+    }
+    public void ratesForRegularCustomer(){
+        System.out.println("1. Lakewood\nWeekday : $110\tWeekend : 90\n\n" +
+                "2.Bridgewood\nWeekday : $160\tWeekend : 60\n\n" +
+                "3.Ridgewood\nWeekday : $220\tWeekend : 150\n");
     }
 }
---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Main {
+    static HotelReservation hotelReservation = new HotelReservation();
+    public static void main(String[] args) {
+        hotelReservation.ratesForRegularCustomer();
+    }
+}
